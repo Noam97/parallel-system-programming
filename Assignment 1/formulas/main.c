fd5e98ba53221f2b5e85ed1e71549a4859c5d062
Noam Lahmani <76108244+Noam97@users.noreply.github.com>
2024-02-06 14:12:24 +0200

    Update main.c

--- Assignment 1/formulas/main.c
+++ Assignment 1/formulas/main.c
@@ -6,7 +6,7 @@
 
 
 
-#define NUM_TESTS 500
+#define NUM_TESTS 100
 #define MAX_LENGTH 10000
 #define MAX_ERROR 0.0001
 #define MIN_VAL 0.00001
@@ -21,15 +21,25 @@ int is_close2(float f1, float f2) {
 }
 
 
+float formula1_test(float* x, unsigned int length) {
+    float sum = 0;
+    float product = 1;
+    for (int k = 0; k < length; k++) {
+        sum += sqrtf(x[k]);
+        product *= (x[k] * x[k] + 1);
+    }
+    return sqrtf(1 + cbrtf(sum) / product);
 
-float formula2_test(float *x, float *y, unsigned int length) {
+}
+
+float formula2_test(float* x, float* y, unsigned int length) {
     float sum = 0;
     for (int k = 0; k < length; k++) {
         float product = 1;
         for (int i = 0; i < length; i++) {
-            product *= (x[i]*x[i] + y[i]*y[i] - 2*x[i]*y[i] + 1);
+            product *= (x[i] * x[i] + y[i] * y[i] - 2 * x[i] * y[i] + 1);
         }
-        sum += (x[k]*y[k])/product;
+        sum += (x[k] * y[k]) / product;
     }
     return sum;
 }
@@ -38,48 +48,53 @@ float formula2_test(float *x, float *y, unsigned int length) {
 int main(void) {
     srand(time(NULL));
 
-    
-    float *x = malloc(sizeof(float) * MAX_LENGTH);
-    float *y = malloc(sizeof(float) * MAX_LENGTH);
+
+    float* x = malloc(sizeof(float) * MAX_LENGTH);
+    float* y = malloc(sizeof(float) * MAX_LENGTH);
 
     for (int i = 0; i < NUM_TESTS; i++) {
         unsigned int length = (rand() % MAX_LENGTH) + 1;
 
         float product;
-        if (length%4 == 0){
-        do {
-            for (unsigned int k = 0; k < length; k++) {
-                x[k] = (((float)rand())/((float)RAND_MAX)) * (MAX_VAl - MIN_VAL) + MIN_VAL;
-                y[k] = (((float)rand())/((float)RAND_MAX)) * (MAX_VAl - MIN_VAL) + MIN_VAL;
-            }
+        if (length % 4 == 0) {
+            do {
+                for (unsigned int k = 0; k < length; k++) {
+                    x[k] = (((float)rand()) / ((float)RAND_MAX)) * (MAX_VAl - MIN_VAL) + MIN_VAL;
+                    y[k] = (((float)rand()) / ((float)RAND_MAX)) * (MAX_VAl - MIN_VAL) + MIN_VAL;
+                }
 
-            product = 1;
-            for (int i = 0; i < length; i++) {
-                product *= (x[i]*x[i] + y[i]*y[i] - 2*x[i]*y[i] + 1);
-            }
+                product = 1;
+                for (int i = 0; i < length; i++) {
+                    product *= (x[i] * x[i] + y[i] * y[i] - 2 * x[i] * y[i] + 1);
+                }
 
-            
-        } while (isnanf(((float)1)/product));
+
+            } while (isnanf(((float)1) / product));
         }
-        else{
+        else {
             continue;
         }
-        
 
+        float form1 = formula1(x, length);
+        float test1 = formula1_test(x, length);
         float form2 = formula2(x, y, length);
         float test2 = formula2_test(x, y, length);
 
- 
+        if (!is_close(form1, test1)) {
+            printf("failed formula 1\n");
+            printf("%f %f\n", form1, test1);
+            return 0;
+        }
 
         if (!is_close(form2, test2)) {
             printf("failed formula 2\n");
-            printf("%f %f\n", form2, test2);
+            printf("%f %f\n", form1, test1);
             is_close2(form2, test2);
 
             return 0;
         }
 
-        printf("test2:%f\n",test2);
+        printf("test1: %f\t\t\t test2:%f\n", test1, test2);
     }
     printf("test completed successfully\n");
 
